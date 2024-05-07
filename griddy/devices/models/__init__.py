6905a041897ecf2d29c5cf52187c082029770d43
Mathis1993 <m_eric01@uni-muenster.de>
2024-05-07 17:03:16 +0200

    #6 Do not use relative imports to avoid confusing the test runner

--- griddy/devices/models/__init__.py
+++ griddy/devices/models/__init__.py
@@ -1,11 +1,14 @@
 __all__ = [
     # base_models.py
     "Action",
+    "Address",
     "Command",
     "CommandLog",
     "Device",
     "Manufacturer",
     # heat_pumps.py
+    "DummyHeatPump",
 ]
 
-from .base_models import Action, Command, CommandLog, Device, Manufacturer
+from devices.models.base_models import Action, Address, Command, CommandLog, Device, Manufacturer
+from devices.models.heat_pumps import DummyHeatPump
