4dd18e5b2a1a32267962cff3583bcd35a1103d53
Mathis1993 <m_eric01@uni-muenster.de>
2024-05-10 14:36:01 +0200

    #6 Add smartthings api token and device id test settings

--- config/settings/test.py
+++ config/settings/test.py
@@ -10,9 +10,10 @@ TEST_USER_PASSWORD = "password1"
 
 # python manage.py runserver 0.0.0.0:8000 and local machine's host name in ALLOWED_HOSTS
 # to connect from phone via host-name:8000/...
-ALLOWED_HOSTS = [
-    "localhost",
-    "baby-yoda"
-]
+ALLOWED_HOSTS = ["localhost", "baby-yoda"]
 
 # FROM_EMAIL = "test@test.de"
+
+# Smartthings API (replace with actual values to record vcr cassettes)
+TEST_SMARTTHINGS_API_TOKEN = "test_token"
+TEST_SMARTTHINGS_DEVICE_ID = "test_device_id"
