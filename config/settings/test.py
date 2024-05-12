bb59c48a87949526ce1227cf032eb31ff06f3ea4
Mathis1993 <m_eric01@uni-muenster.de>
2024-05-12 17:55:21 +0200

    #14 Adapt host name used for mobile device testing

--- config/settings/test.py
+++ config/settings/test.py
@@ -10,7 +10,7 @@ TEST_USER_PASSWORD = "password1"
 
 # python manage.py runserver 0.0.0.0:8000 and local machine's host name in ALLOWED_HOSTS
 # to connect from phone via host-name:8000/...
-ALLOWED_HOSTS = ["localhost", "baby-yoda"]
+ALLOWED_HOSTS = ["localhost", "baby-yoda.local"]
 
 # FROM_EMAIL = "test@test.de"
 
