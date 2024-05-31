d500066b80902583ba388794c6ff6595d466d133
Mathis1993 <m_eric01@uni-muenster.de>
2024-05-31 18:12:08 +0200

    #22 Add test skeletons for tests that are next up for implementation

---
+++ griddy/devices/models/tests/test_time_control.py
@@ -0,0 +1,8 @@
+def test_time_profile_get_current_time_slot():
+    # ToDo(ME-31.05.24):
+    pass
+
+
+def test_time_slot_get_current_target_value():
+    # ToDo(ME-31.05.24):
+    pass
---
+++ griddy/netzentgelte/tests/test_models.py
@@ -0,0 +1,3 @@
+def test_netzentgelt_get_currently_valid_netzentgelt():
+    # ToDo(ME-31.05.24):
+    pass
