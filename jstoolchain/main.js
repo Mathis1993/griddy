ce2686414069f4004639b6f6b12202ef8868ffb2
Mathis1993 <m_eric01@uni-muenster.de>
2024-12-29 20:59:50 +0100

    feat: Include full page scroll on landing page

--- jstoolchain/main.js
+++ jstoolchain/main.js
@@ -2,6 +2,11 @@ import 'preline';
 
 import { HSSelect } from 'preline/dist/preline';
 
+import Swiper from 'swiper';
+import { Mousewheel, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 // Initialize HSSelect
 document.addEventListener('DOMContentLoaded', () => {
     HSSelect.autoInit();
@@ -11,3 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
 document.addEventListener('htmx:afterSwap', () => {
     HSSelect.autoInit();
 });
+
+// Make Swiper available globally
+window.Swiper = Swiper;
+window.SwiperModules = { Mousewheel, Pagination };
